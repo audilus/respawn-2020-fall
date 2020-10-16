437f3b4302acc34ee8cb3e41ad42e098ab58c147
Koi Hamilton <hamiltonkoi5@gmail.com>
2020-10-15 22:59:32 -0400

    import standard assets, start work on player

---
+++ Assets/Scripts/MoveUP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") != 0){
+            transform.Translate(new Vector3(0, Input.GetAxisRaw("Vertical") * Time.deltaTime * 60f,0));
+        }else{
+            transform.Translate((Vector3.zero - transform.position) * Time.deltaTime);
+        }
+    }
+}
