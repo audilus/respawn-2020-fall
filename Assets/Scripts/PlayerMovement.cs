3211f15dd8c0fd6ee3c13188747f04de667f55ce
Koi Hamilton <hamiltonkoi5@gmail.com>
2020-10-16 00:52:48 -0400

    update player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,18 @@ public class PlayerMovement : MonoBehaviour
 
     public float acceleration = 10f;
     public float maxFloorVelocity = 90f;
-    public float maxVelocity = 400f;
+    public float maxAirVelocity = 30f;
     public float floorVelocityDampening = 0.7f;
-    public float airVelocityDampening = 0.9f;
     public float jumpPower = 4f;
     public float gravityMult = 4f;
+    public float airAccel = 0.1f;
+    public float airTimeLimit = 2f;
     public int jumpFrameDelay = 5;
 
     private Rigidbody rigidbody;
     private bool isGrounded = true;
     private int jumpDelay = 0;
+    private float airTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,29 +29,40 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called every physics tick (locked at 60Hz), so I don't need to multiply by time.deltatime here
     void FixedUpdate()
     {
-        Vector3 final = new Vector3(rigidbody.velocity.x * airVelocityDampening, rigidbody.velocity.y, rigidbody.velocity.z * airVelocityDampening);
+        
+    }
+
+    void Update()
+    {
+        Vector3 final = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z);
         jumpDelay--;
+
+
 
         if(isGrounded){
             final = new Vector3(rigidbody.velocity.x * floorVelocityDampening, rigidbody.velocity.y, rigidbody.velocity.z * floorVelocityDampening);
         }
+        
         if ((final.magnitude <= maxFloorVelocity) && isGrounded)
         {
             final += Input.GetAxis("Vertical") * transform.forward * acceleration;
             final += Input.GetAxis("Horizontal") * transform.right * acceleration;
         }
+        // else if ((final.magnitude <= maxAirVelocity) && airTime > 0)
+        // {
+        //     final += Input.GetAxis("Vertical") * transform.forward * (airAccel * airTime + 1);
+        //     final += Input.GetAxis("Horizontal") * transform.right * (airAccel * airTime );    
+        // }
 
-        final += Vector3.down * gravityMult;
+        airTime -= Time.deltaTime;
+        Debug.Log(airTime);
+
+        final += Vector3.down * gravityMult * Time.deltaTime;
         rigidbody.velocity = final;
-    }
-
-    void Update()
-    {
 
         if (Input.GetButton("Jump") && isGrounded && (jumpDelay <= 0))
         {
             rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-            Debug.Log("afsek;");
             jumpDelay = jumpFrameDelay;
         }
         else
@@ -57,10 +70,13 @@ public class PlayerMovement : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f))
             {
-                if (hit.collider.enabled)
+                if (hit.collider.enabled){
                     isGrounded = true;
-                else
+                    airTime = airTimeLimit;
+                }   
+                else{
                     isGrounded = false;
+                }
             }
             else
             {
