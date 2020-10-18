245f69d6d83dced575f0b73c28474e5d929aea9b
45Angle <48843607+45Angle@users.noreply.github.com>
2020-10-18 04:42:29 -0400

    So for lv3 and lv4 I can't do the lighting but they are mostly done. Also the wall on lv4 does not work but im not sure why. Will try and continuie more levels in the morning

--- Assets/Scripts/moveUp.cs
+++ Assets/Scripts/moveUp.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Net.Sockets;
-using System.Security.Cryptography;
 using UnityEngine;
 
-public class moveUp : MonoBehaviour
+public class MoveUP : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,43 +13,10 @@ public class moveUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetAxisRaw("Vertical") <   0){
-            transform.Translate(new Vector3(0, 0, -2.25f * Time.deltaTime));
-        }
-        if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            transform.Translate(new Vector3(0, 0, 2.25f * Time.deltaTime));
-        }
-         if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            transform.Translate(new Vector3(-2.25f * Time.deltaTime, 0, 0));
-        }
-         if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            transform.Translate(new Vector3(2.25f * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetAxisRaw("Jump") > 0)
-        {
-            if (transform.position.y <= 0)
-            {
-                transform.Translate(new Vector3(0, 6f * Time.deltaTime, 0));
-            }
-            /*else
-            {
-                if (transform.position.y > 0)
-                {
-                    transform.Translate(new Vector3(0, -1.25f * Time.deltaTime, 0));
-                }
-            }*/
-        }
-        else
-        {
-            if (transform.position.y > 0)
-            {
-                transform.Translate(new Vector3(0, -1.25f * Time.deltaTime, 0));
-            }
+        if(Input.GetAxis("Vertical") != 0){
+            transform.Translate(new Vector3(0, Input.GetAxisRaw("Vertical") * Time.deltaTime * 60f,0));
+        }else{
+            transform.Translate((Vector3.zero - transform.position) * Time.deltaTime);
         }
     }
-
 }
