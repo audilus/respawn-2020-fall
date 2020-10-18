968a5ff7293612b68f4d41d7693e943667578654
Koi Hamilton <hamiltonkoi5@gmail.com>
2020-10-18 17:38:01 -0400

    made a main menu, added some music (thanks, volant!)

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+    AudioSource source;
+    public float volume = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.volume = volume;
+    }
+    
+    public void ChangeVol(float v)
+    {
+        volume = v;
+    }
+
+}
